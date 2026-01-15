6ef2508613e80d3837c6e67558ad96c0b83f5720
daniloslay1990-lgtm <daniloslay1990@gmail.com>
2026-01-15 15:27:46 -0500

    Codigo del programa

--- Sistema_Vehiculos/Modelos/vehiculo_auto.py
+++ Sistema_Vehiculos/Modelos/vehiculo_auto.py
@@ -1,11 +1,9 @@
+# Clase derivada para automóviles
+
 from Sistema_Vehiculos.Modelos.vehiculo import Vehiculo
 
 class VehiculoAuto(Vehiculo):
-    """
-    Clase derivada para automóviles.
-    Aplica polimorfismo al calcular el consumo.
-    """
-
+    
     def __init__(self, marca, kilometros, rendimiento):
         super().__init__(marca, kilometros)
         self.rendimiento = rendimiento  # km por litro
